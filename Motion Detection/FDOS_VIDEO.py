e564791cc0a49259c77e240560cecb3769dd5dc4
1SA • DEV⚡ <96635023+Rapunzel-ware@users.noreply.github.com>
2023-04-21 22:26:01 +0200

    Update FDOS_VIDEO.py

--- Motion Detection/FDOS_VIDEO.py
+++ Motion Detection/FDOS_VIDEO.py
@@ -1,33 +1,22 @@
 import cv2
 import numpy as np
 
-# Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Define the video
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture('video.mp4')
 
 while True:
-    # Read the frame
     _, frame = cap.read()
-
-    # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-    # Draw rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    # Show the frame
-    cv2.imshow('frame', frame)
 
-    # Stop if escape key is pressed
+    cv2.imshow('frame', frame)
     k = cv2.waitKey(30) & 0xff
     if k==27:
         break
 
-# Release the VideoCapture
+
 cap.release()
